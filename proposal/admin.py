a72cdbd540bdaa85c60b339944fe152b426b91c3
Holger Karl <hkarl@ieee.org>
2017-03-08 16:17:50 +0100

    make debugging easier by admin integration

--- proposal/admin.py
+++ proposal/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
-from proposal.models import Workpackage
+from proposal.models import *
 
 class WorkpackageAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Workpackage, WorkpackageAdmin)
+admin.site.register(Textblock)
+admin.site.register(Partner)
+admin.site.register(Task)
+admin.site.register(ProducableTypes)
+admin.site.register(DisseminationTypes)
+admin.site.register(Deliverable)
+admin.site.register(Milestone)
+admin.site.register(TaskPartnerPM)
+admin.site.register(DeliverablePartnerTaskPM)
+admin.site.register(MilestonePartnerTaskPM)
+admin.site.register(Project)
+admin.site.register(Setting)
+admin.site.register(Template)
+
 
